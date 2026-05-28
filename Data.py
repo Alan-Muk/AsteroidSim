a2aa41849a20e26abd8baf80bbf634d9eaad64ef
Allan <95118744+Alan-Muk@users.noreply.github.com>
2026-05-28 07:39:14 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,3 +5,15 @@ data = pd.read_csv("MPCORB.csv")
 
 # Keep only necessary columns
 asteroids = data[['a','e','i','node','peri','M']]
+
+"""
+Loads asteroid orbital data from the MPCORB catalog.
+
+Selected orbital elements:
+a     = semi-major axis
+e     = eccentricity
+i     = inclination
+node  = longitude of ascending node
+peri  = argument of perihelion
+M     = mean anomaly
+"""
